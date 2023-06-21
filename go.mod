76cbe361220400c6fbef11d4a439a77d04c091f5
rayoneitalom <rayoneitalo@gmail.com>
2023-06-21 17:59:47 -0300

    feat: first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/italorayonne-study/fullcycle-challange-docker-go
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,12 @@
+/**
+ * @author: rayoneitalo
+ * @description: Run main file
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Full Cycle Rocks!!")
+}
